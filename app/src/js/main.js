4657b593222e417f94ee3977925ba331433348c4
MarcosToledo <xquinhox@gmail.com>
2019-09-28 10:22:51 -0300

    criando function createLocalStorage

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -4,22 +4,39 @@ mLabs = (function() {
   'use strict';
 
   function init() {
-    getApi();
     startModule();
-  }
-
-  function getApi() {
-    $.get("https://demo2697181.mockable.io/pages", function(data, status){
-      var data = JSON.stringify(data.data);
-
-      console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-    });
+    getApi();
   }
 
   function startModule() {
     $.each(mLabs.modules, function(i, module) {
       mLabs[module].init();
       $.event.trigger(module,':ready', [i, module]);
+    });
+  }
+
+  function getApi() {
+    $.get("https://demo2697181.mockable.io/pages", function(data, status){
+      var data = data.data;
+      
+      createLocalStorage(data);
+    });
+  }
+
+  function createLocalStorage(data) {
+    var socialNetworks = [
+      {socialName: 'facebook', pages: []},
+      {socialName: 'twitter', pages: []},
+      {socialName: 'instagram', pages: []},
+      {socialName: 'google_analytics', pages: []},
+      {socialName: 'pinterest', pages: []},
+      {socialName: 'youtube', pages: []},
+    ];
+
+    $.each(socialNetworks, function(i, socialNetwork) {
+      var socialName = socialNetwork.socialName;
+      
+      socialNetwork.pages = data.filter(function(obj) { return obj.channel_key === socialName });
     });
   }
 
